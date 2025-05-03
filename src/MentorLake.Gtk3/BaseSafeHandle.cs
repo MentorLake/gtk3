a5dd28c315ed78904e5ee7c405b7a1a6653e8937
jmis <324872+jmis@users.noreply.github.com>
2025-05-03 16:36:51 -0400

    Safe handle equality oeprator overload

--- src/MentorLake.Gtk3/BaseSafeHandle.cs
+++ src/MentorLake.Gtk3/BaseSafeHandle.cs
@@ -7,4 +7,34 @@ public class BaseSafeHandle : SafeHandleZeroOrMinusOneIsInvalid
 	public BaseSafeHandle() : base(true) { }
 
 	protected override bool ReleaseHandle() => true;
+
+	public static bool operator ==(BaseSafeHandle left, BaseSafeHandle right)
+	{
+		if (ReferenceEquals(left, null) && ReferenceEquals(right, null))
+			return true;
+		if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+			return false;
+
+		return left.DangerousGetHandle() == right.DangerousGetHandle();
+	}
+
+	public static bool operator !=(BaseSafeHandle left, BaseSafeHandle right)
+	{
+		return !(left == right);
+	}
+
+	public override bool Equals(object obj)
+	{
+		if (obj is BaseSafeHandle other)
+		{
+			return this == other;
+		}
+
+		return false;
+	}
+
+	public override int GetHashCode()
+	{
+		return DangerousGetHandle().GetHashCode();
+	}
 }
